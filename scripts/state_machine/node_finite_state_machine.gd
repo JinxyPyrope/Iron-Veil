81bfa182e5ccecbbafa050abe80a7b413d0cbd14
JinxyPyrope <jinxypy@yahoo.com>
2025-02-05 12:43:29 -0500

    Setting up foundation for state machine and node state

--- scripts/state_machine/node_finite_state_machine.gd
+++ scripts/state_machine/node_finite_state_machine.gd
@@ -1,11 +1,44 @@
-extends Node
+class_name StateMachine extends Node
+
+## The initial state of the state machine. If not set, the first child node is used.
+@export var initial_state: State = null
+
+## The current state of the state machine.
+@onready var state: State = (func get_initial_state() -> State:
+	return initial_state if initial_state != null else get_child(0)
+).call()
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Give every state a reference to the state machine.
+	for state_node: State in find_children("*", "State"):
+		state_node.finished.connect(_transition_to_next_state)
 
+	# State machines usually access data from the root node of the scene they're part of: the owner.
+	# We wait for the owner to be ready to guarantee all the data and nodes the states may need are available.
+	await owner.ready
+	state.enter("")
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
+	state.handle_input(event)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	state.update(delta)
+
+
+func _physics_process(delta: float) -> void:
+	state.physics_update(delta)
+	
+#Changes the active state when the state emits the signal. 
+#Because we use nodes for states, we can check if a state exists 
+#using has_node() before transitioning to it.
+func _transition_to_next_state(target_state_path: String, data: Dictionary = {}) -> void:
+	if not has_node(target_state_path):
+		printerr(owner.name + ": Trying to transition to state " + target_state_path + " but it does not exist.")
+		return
+
+	var previous_state_path := state.name
+	state.exit()
+	state = get_node(target_state_path)
+	state.enter(previous_state_path, data)
